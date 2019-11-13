0234cc6f4888c8d7a029053d921f7460b030ffaa
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2019-11-13 05:20:54 -0600

    adds store picker component and styling

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import "./css/style.css";
+
+render(
+  <p>
+    <StorePicker />
+  </p>,
+  document.querySelector("#main")
+);
