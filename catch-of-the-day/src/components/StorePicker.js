f30236b2aa374413c6520fd3a2d7a5e7576b2d46
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2019-11-17 20:14:52 -0600

    adds redirect to store page on submit

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  storePickerInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.storePickerInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.storePickerInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Vist Store >></button>
       </form>
     );
