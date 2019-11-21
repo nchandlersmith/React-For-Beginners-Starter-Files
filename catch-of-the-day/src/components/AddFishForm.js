594b86756ba1d0a175b48ce60bc3f7d474e409d4
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2019-11-21 05:14:58 -0600

    price is a float

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -11,7 +11,7 @@ class AddFishForm extends React.Component {
     event.preventDefault();
     const fish = {
       name: this.nameRef.current.value,
-      price: this.priceRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
       status: this.statusRef.current.value,
       description: this.descriptionRef.current.value,
       image: this.imageRef.current.value
