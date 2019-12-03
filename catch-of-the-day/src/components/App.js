2f767c14d81d39bb98985d11ead92eda13d862f1
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2019-12-03 17:54:55 -0600

    adds order to store

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -34,7 +34,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                fishKey={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
