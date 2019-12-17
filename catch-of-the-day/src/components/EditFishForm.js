e4c0580a82257f38ba4a5fddb239e5d21cf75f95
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2019-12-17 05:15:57 -0600

    adds edit fish form compopnent

---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+class EditFishForm extends React.Component {
+  handleChange = event => {
+    console.log(event.currentTarget.name);
+    console.log(event.currentTarget.value);
+    const updatedFish = { ...this.props.fish };
+    console.log(updatedFish);
+  };
+  render() {
+    return (
+      <div className="fish-edit">
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.props.fish.name}
+        />
+        <input
+          type="text"
+          name="price"
+          onChange={this.handleChange}
+          value={this.props.fish.price}
+        />
+        <select
+          type="text"
+          name="status"
+          onChange={this.handleChange}
+          value={this.props.fish.status}
+        >
+          <option onChange={this.handleChange} value="available">
+            Fresh!
+          </option>
+          <option onChange={this.handleChange} value="unavailable">
+            Sold Out!
+          </option>
+        </select>
+        <textarea
+          type="text"
+          name="description"
+          onChange={this.handleChange}
+          value={this.props.fish.desc}
+        />
+        <input
+          type="text"
+          name="image"
+          onChange={this.handleChange}
+          value={this.props.fish.image}
+        />
+      </div>
+    );
+  }
+}
+
+export default EditFishForm;
